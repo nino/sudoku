e93219c9c15b5cce7ab263be1997fd64e6dd6c6d
Nino Annighoefer <nino@ninoan.com>
2023-01-30 13:49:24 +0000

    Rearrange

--- bin/main.ml
+++ bin/main.ml
@@ -5,7 +5,6 @@ let styles = {|
   }
 |}
 
-
 let css_provider_from_data data =
   let provider = GObj.css_provider () in
   provider#load_from_data data;
@@ -23,6 +22,11 @@ let solve fname =
       Sudoku.Board.print_board solved;
       Printf.printf "\n\n"
   | Error err -> Printf.printf "Error: %s\n\n%!" err
+
+let () =
+  solve "./assets/board1.txt";
+  solve "./assets/board2.txt";
+  solve "./assets/board3.txt"
 
 let gui_main () =
   let _ = GMain.init () in
@@ -44,26 +48,5 @@ let gui_main () =
 
   window#show ();
   GMain.main ()
-
-let () =
-  solve "./assets/board1.txt";
-  solve "./assets/board2.txt";
-  solve "./assets/board3.txt"
-
-(* let gui_main () = *)
-(*   let _ = GMain.init () in *)
-(*   let window = GWindow.window ~title:"Sudoku" ~border_width:10 () in *)
-(*   let _ = window#connect#destroy ~callback:GMain.quit in *)
-(*   let hbox = GPack.hbox ~packing:window#add () in *)
-(*   (1* let button = GButton.button ~label:"Butto!" ~packing:hbox#pack () in *1) *)
-(*   (1* let _ = button#connect#clicked ~callback:GMain.quit in *1) *)
-(*   let board_model = *)
-(*     In_channel.(with_open_text "./assets/board3.txt" input_all) *)
-(*     |> Sudoku.of_string |> Result.get_exn *)
-(*   in *)
-(*   let board = Components.board board_model ~packing:hbox#pack () in *)
-(*   ignore board; *)
-(*   window#show (); *)
-(*   GMain.main () *)
 
 let _ = gui_main ()
